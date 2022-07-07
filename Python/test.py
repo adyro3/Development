4b98a82ff1fa4cd0c51d858e87ea09bf68303ad3
Adrian <adrianadrian198@gmail.com>
2022-07-07 18:44:21 +0300

    pygame

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,27 @@
-a = 5
-b = 6
-print(a+b)
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+screen = pygame.display.set_mode(
+    (SCREEN_WIDTH, SCREEN_HEIGHT))  # set window size
+pygame.display.set_caption("Python Platformer")  # set window name
+
+
+
+running = True
+while running:
+
+    screen.fill((26, 199, 190))
+
+   # player.draw()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+pygame.quit()
